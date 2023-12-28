549bad7b6f14600afd2034de40baeba8d91d1e70
soma0000 <137361433+soma0000@users.noreply.github.com>
2023-12-28 17:30:10 +0900

    コメントを追加
    commit c22ee0c9204d5b6b653825288ba31c79c411b0d0
    Author: soma0000 <137361433+soma0000@users.noreply.github.com>
    Date:   Thu Dec 28 17:28:40 2023 +0900
    
        コメント追加
    
        This reverts commit 912b66d1d5550240c4fbca22e03048ce787e027b.

--- Source/DebugModule/Public/DebugComponent.h
+++ Source/DebugModule/Public/DebugComponent.h
@@ -70,21 +70,14 @@ public:
 class IDebugModule : public IModuleInterface
 {
 public:
-	/**
-	 * Singleton-like access to this module's interface.  This is just for convenience
-	 * Beware of calling this during the shutdown phase, though.  Your module might have been unloaded already.
-	 *
-	 * @return Returns singleton instance, loading the module on demand if needed
-	 */
+	
+	// シングルインスタンスを返す。必要な時にロード
 	static inline IDebugModule& Get()
 	{
 		return FModuleManager::LoadModuleChecked< IDebugModule >("DebugModule");
 	}
-	/**
-	 * Checks to see if this module is loaded and ready.  It is only valid to call Get() if IsAvailable() returns true.
-	 *
-	 * @return True if the module is loaded and ready to use
-	 */
+	
+	// Moduleがロードされ、準備ができたか　使用可能ならTrue
 	static inline bool IsAvailable()
 	{
 		return FModuleManager::Get().IsModuleLoaded("DebugModule");
